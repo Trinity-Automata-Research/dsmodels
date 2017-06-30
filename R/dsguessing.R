87bd55bc670e70c7bc081d8e8a6adf25c3f0c01b
cstein1 <cstein1@trinity.edu>
2017-06-30 13:22:50 -0500

    Small typo

--- R/dsguessing.R
+++ R/dsguessing.R
@@ -182,7 +182,7 @@ is.dsimage <- function(x) inherits(x,"image")
 #' that non-attractor fixpoints will be found by accident. The function
 #' is iterated until the points move less than \code{stable} (default
 #' \code{sqrt(.Machine$double.eps)}) between iterations.
-#' The color of each point is drawn from the \code{col} parameter. If the number of poitns exceeds
+#' The color of each point is drawn from the \code{col} parameter. If the number of points exceeds
 #' the size of $\code{col}$, or $\code{col}$ is not defined, then reasonable defaults are used instead.
 #' The attractors are \code{dspoint}s that are added to the model.
 #'
