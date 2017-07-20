d02bcdc1020b76cb4390e4beb1f06125b351e8a9
Charles Stein <cstein1@trinity.edu>
2017-07-20 13:41:53 -0500

    Added a test case for LaTeX code in the title

---
+++ Examples/TestCases/TestCase46_LaTex_Text_Input.R
@@ -0,0 +1,21 @@
+library(latex2exp)
+rm(list=ls())
+
+
+K <- 2.05
+L <- 2.05
+a <- 0.5
+b <- 0.5
+
+fun <- function(x,y) {
+  xp <- x*exp(K-x-a*y)
+  yp <- y*exp(L-y-b*x)
+  list(xp,yp)
+}
+
+xstar = (K - a*L)/(1-a*b)
+ystar = (L - b*K)/(1-a*b)
+
+dsmodel(fun, title = "Hello, $\\LaTeX$") + dsrange(2,1.1, discretize = 1) + xlabel("$\\alpha$ vs $\\beta$", col = "green") +
+  ylabel("$\\heartsuit$", col = "red")+
+  dspoint(1,1, label="$x^{-x+1}$")
