b0d4f291d0b8aadca9c146453da7154e3f0343ae
kmclaren-trinity <kmclaren-trinity>
2018-06-05 14:27:54 -0500

    adding tests to simmapperiod

--- Examples/simmapperiod.R
+++ Examples/simmapperiod.R
@@ -1,5 +1,5 @@
 simmapperiod = function(testX,testY, alim=NULL, blim=NULL, discretize=0, xlim=NULL, ylim=NULL,cols=NULL,
-                display= NULL, initIters=1000, maxPeriod=128, numTries=1,
+                display= TRUE, initIters=1000, maxPeriod=128, numTries=1,
                 epsilon=sqrt(sqrt(.Machine$double.eps)), rangeMult=0){
   dsproto(
     `_class` = "image", `_inherit` = background, #what should this inherit?
@@ -50,8 +50,10 @@ simmapperiod = function(testX,testY, alim=NULL, blim=NULL, discretize=0, xlim=NU
       self$colMatrix=matrix(z,length(self$grid$x))
     },
     render = function(self, model){
-      dsassert(self$bound,"attempting to render bifmap before bound", critical = TRUE)
-      image(self$grid$x,self$grid$y, self$colMatrix, zlim = c(0, length(self$cols)-1), col=self$cols)
+      dsassert(self$bound,"simmapperiod: attempting to render bifmap before bound", critical = TRUE)
+      if(display){
+        image(self$grid$x,self$grid$y, self$colMatrix, zlim = c(0, length(self$cols)-1), col=self$cols)
+      }
     }
   )
 }
@@ -65,7 +67,27 @@ dsassert = function(t,str,critical=FALSE) {
   }
 }
 
+#test cases
 
+#add range first, no defaults in sim (should give no warnigs about assuming divergence, red and blue)
 m=dsmodel( function(x,y,a,b){list(a*x,b*y)})
-m+paramrange(a=2,b=2,paramdiscretize = .5)
-m+simmapperiod(1,1,discretize = .5)
+m+paramrange(a=2,b=2,x=2,y=2,paramDiscretize = 1)
+m+simmapperiod(1,1,rangeMult=3)
+
+#FIXME------- if model has a range, x/ylims dont get overidden. not that important.
+#add range first, defaults in sim (should give warnigs about assuming divergence, only show blue)
+m=dsmodel( function(x,y,a,b){list(a*x,b*y)})
+m+paramrange(a=2,b=2,x=2,y=2,paramdiscretize = 1)
+m+simmapperiod(1,1,alim=1,blim=1,xlim=Inf,ylim=Inf,discretize = .5)
+
+#dont add range, no defaults in sim (should crash)
+m=dsmodel( function(x,y,a,b){list(a*x,b*y)})
+m+simmapperiod(1,1)
+
+#dont add range, param defaults in sim, but no x,y defaults (red and blue)
+m=dsmodel( function(x,y,a,b){list(a*x,b*y)})
+m+simmapperiod(1,1,alim=2,blim=2,discretize = 1)
+
+#dont add range, param defaults and x,y defaults in sim (should give no warnigs about assuming divergence, red and blue)
+m=dsmodel( function(x,y,a,b){list(a*x,b*y)})
+m+simmapperiod(1,1,alim=2,blim=2, xlim=2,ylim=2,discretize = 1, rangeMult = 3)
