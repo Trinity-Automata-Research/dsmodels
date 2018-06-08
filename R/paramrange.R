43266283c12497b0a459e6afbcf3d19cb38d9031
kmclaren-trinity <kmclaren-trinity>
2018-06-07 20:53:44 -0500

    fixing a few bugs in sim.map.period and paramrange

--- R/paramrange.R
+++ R/paramrange.R
@@ -49,9 +49,9 @@
 #' model + paramrange(3, 3, discretize = .09, paramNames=c(a,b))
 
 paramrange = function(alim,blim,xlim=0,ylim=0, paramNames=NULL,discretize=0, renderCount=101, axes = TRUE, frame.plot = TRUE, ...){
-  ds=dsrange(x,y,discretize, renderCount=renderCount, axes = axes, frame.plot = frame.plot, ...=...)
-  alim = make.lims(a)
-  blim = make.lims(b)
+  ds=dsrange(xlim,ylim,discretize, renderCount=renderCount, axes = axes, frame.plot = frame.plot, ...=...)
+  alim = make.lims(alim)
+  blim = make.lims(blim)
   givenNames = substitute(paramNames)
   if(safe.apply(is.null,paramNames)) {
     aname <- NULL
