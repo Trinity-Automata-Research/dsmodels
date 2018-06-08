8072e4c7ab1f9e861c33913d5815fddc3ba51af4
kmclaren-trinity <kmclaren-trinity>
2018-06-08 13:44:03 -0500

    fixing links in paramrange's description

--- R/paramrange.R
+++ R/paramrange.R
@@ -25,7 +25,7 @@
 #'  made for which arguments of the function are the parameters to vary, usualy the third and fourth arguments.
 #' @param discretize If a value is provided, the field is discretized into an array of points. The value
 #'  specifies the distance between each point.
-#'  This becomes the default when displaying \code{\link{sim-map-period}}
+#'  This becomes the default when displaying \code{\link{sim.map.period}}
 #' @param axes If \code{FALSE}, the axes will not be drawn. Defaults to \code{TRUE}.
 #' @param frame.plot If \code{FALSE}, the frame of the plot will not be drawn. Defaults to \code{TRUE}.
 #'  \deqn{(xmax-xmin+1)(ymax-ymin+1)/discretize.}{ascii}
@@ -35,7 +35,7 @@
 #'   displayed. Default 101.
 #' @param ... Further fields for the dsrange object.
 #' @seealso \code{\link{dsmodel}}
-#' @seealso \code{\link{sim-map-period}}
+#' @seealso \code{\link{sim.map.period}}
 #' @export
 #' @examples
 #' fun <- function(X,Y,a,b) {
