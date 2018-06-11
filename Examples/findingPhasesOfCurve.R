146db2acb3c86575064b79c0c2076e9780423fab
kmclaren-trinity <kmclaren-trinity>
2018-06-11 13:42:22 -0500

    making segments

---
+++ Examples/findingPhasesOfCurve.R
@@ -0,0 +1,51 @@
+#make points
+#similar to dscruve
+f=function(x,y,a=.5,b=.5,s=1,r=1,dummy=0){
+  list(x*exp(r-x-a*y),
+       y*exp(s-b*x-y))
+}
+model=dsmodel(f)
+range = paramrange(alim=3,blim=3,paramNames = c(s,r))
+model + range
+#curve=dscurve(function(x)x/2, display = TRUE)
+curve=dscurve(x/2, n=100, xlim=c(2,2.71))
+model+curve
+#model + sim.map.period(.5,.5, discretize=.2, maxPeriod = 4, epsilon=.001, iters = 100, numTries = 1, powerOf2=TRUE)
+
+#numPoints=10000
+#amin = min(range$alim)
+#amax = max(range$alim)
+#as=seq(amin,amax,length.out=numPoints)
+#bs=mapply(curve$fun,as)
+testX=1
+testY=1
+args=list(FUN=model$find.period,x=testX,y=testY, numTries=10) #,the rest of args
+args[[range$aname]]=curve$xValues
+args[[range$bname]]=curve$yValues
+periods=do.call(what=mapply,args=args)
+
+#pointsWithPeriods=mapply(c,as,periods)
+
+#bifSpots=list()
+
+#spot=1
+#for(i in 2:length(periods)-1){
+#  if(periods[[i]]!=periods[[i+1]]){
+#    print(c(curve$xValues[[i]],periods[[i]],"to",curve$xValues[[i+1]],periods[[i+1]]))
+#  }
+  #if(periods[[i]]!=periods[[i+1]]){
+  #  bifSpots[[spot]]c(as[[i]],periods[[i]])
+  #  spot=spot+1
+  #}
+#}
+
+
+#turn list of data frames into list of segments
+points = list(x=curve$xValues, y=curve$yValues, periods=periods)
+#list.group(points, periods)
+points=data.frame(points)
+segments=split.data.frame(points,as.factor(periods))
+
+segments=lapply(FUN=function (s) {list(x=segments[[s]]$x, y=segments[[s]]$y, color=as.numeric(s))}, X=names(segments))
+#print(segments)
+
