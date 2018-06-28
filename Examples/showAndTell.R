6fa676964a04e6b197d0e904b39fd31765c78b9e
kmclaren-trinity <kmclaren-trinity>
2018-06-28 16:48:33 -0500

    more testing

--- Examples/showAndTell.R
+++ Examples/showAndTell.R
@@ -40,7 +40,7 @@ model + paramrange(6,6,discretize = .5, paramNames = c(a,b))
 #generate an image based on periodicity tested at the point (.5,.5). Takes a bit of time.
 #maxperiodicity=8 makes every periodicity above 8 count as divergent or 0.
 model + sim.map.period(.5,.5,maxPeriod = 128, epsilon=.0001, iters = 100, numTries = 1, powerOf2=TRUE)
-#this crashes
+#^ test case for issue #140. now working.
 
 
 
