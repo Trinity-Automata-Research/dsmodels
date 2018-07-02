cd0aa5a3c5a696ec3df9d3a22019c366476d6f67
kmclaren-trinity <kmclaren-trinity>
2018-07-02 18:16:44 -0500

    reorganizing dscurve

--- R/dscurve.R
+++ R/dscurve.R
@@ -136,22 +136,20 @@
 dscurve <- function(fun, yfun = NULL,
                     col = "black", image = NULL,
                     lwd = 3, n=NULL, iters = 0, simPeriod=FALSE, find.period.args=list(),
-                    testX=.1, testY=.1,
+                    testX=.1, testY=.1, #better names? simX, simY?
                     crop = FALSE,  tstart=0, tend=1,
                     discretize=FALSE, xlim = NULL, display=TRUE,
                     ...) {
-  colors=NULL
-  if(!simPeriod){
+  if(!simPeriod) {
     colors <- colorVector(col, image, iters)
     iters <- length(colors)-1
+  } else {
+    colors=NULL
   }
 
   fun = substitute(fun)
   yfun = substitute(yfun)
-  if(!safe.apply(is.null,yfun)){ #curve is parametric
-    isParametric=TRUE
-    lims=c(tstart,tend)
-  } else {                       #curve is not parametric
+  if(safe.apply(is.null,yfun)) { #curve is not parametric
     isParametric=FALSE
     if(is.null(xlim)){
       lims=NULL
@@ -159,6 +157,9 @@ dscurve <- function(fun, yfun = NULL,
     else{
       lims=make.lims(xlim)
     }
+  } else { #curve is parametric
+    isParametric=TRUE
+    lims=c(tstart,tend)
   }
 
   dsproto(
@@ -169,8 +170,10 @@ dscurve <- function(fun, yfun = NULL,
     getY=NULL,
     isParametric=isParametric,
     col = colors,
+    colMap = NULL,
     givenColors = col,
-    testX=testX, testY=testY,
+    testX=testX,
+    testY=testY,
     lwd = lwd,
     iters = iters,
     simPeriod=simPeriod,
@@ -187,100 +190,32 @@ dscurve <- function(fun, yfun = NULL,
     display = display,
     ... = ...,
     #functions to interact with the model
-    evaluateFunctions = function(self,model){ #determining how to pick x and y values
-      if(self$isParametric){
-        self$getX <- ensureFunction(self$fun, TRUE)
-        self$getY <- ensureFunction(self$yfun, TRUE)
-        self$sourceName="t"
-      }
-      else{ #not parametric curve
-        getX <- identity
-        getY <- ensureFunction(self$fun, FALSE)
-        if(is.paramrange(model$range)){#parameterized model
-          if(!safe.apply(is.function, eval(self$fun))){ #user did not pass a full function, so getY potentially needs modifying
-            subNames=all.names(self$fun)
-            self$sourceName=model$range$aname
-            ain=self$sourceName %in% subNames
-            xin="x" %in% subNames
-            if(!xin){
-              names(formals(getY))=self$sourceName
-            }
-            else if(ain){
-              names(formals(getY))=self$sourceName
-              warning(paste("curve function contains both 'x' and'", self$sourceName, "'. Assuming you want to vary ",self$sourceName,"."))
-            }
-            else{ #curve function dosent contain 'x' or aname.
-              self$sourceName="x"
-            }
-          }
-          else{ #user passed in a full function
-            self$sourceName=names(formals(getY))[[1]]
-          }
-        }
-        else{ #not parameterized model
-          self$sourceName="x"
-        }
-        self$getX=getX
-        self$getY=getY
-      }
-    },
-    makeSourceSeq = function(self, model){
-      if(is.null(self$n)) #get numPoints
-        numPoints <- model$range$renderCount
-      else
-        numPoints <- self$n
-      if(is.null(self$lims)){ #get limits if we dont have them
-        if(is.paramrange(model$range)) #if we have a paramRange, we want the alim.
-          self$lims=model$range$alim
-        else                           #if not, we want the xlim
-          self$lims=model$range$xlim
-      }
-      from=min(self$lims)  #make a sequence
-      to=max(self$lims)
-      seq(from,to, length.out = numPoints)
-    },
     on.bind = function(self, model) {
       #common between all curves
       self$bound = TRUE
       self$model=model
-      self$sources <-self$makeSourceSeq(model)
-      self$evaluateFunctions(model)
+      self$makeSources(model)
+      self$buildFunctions(model)
       self$xValues <-mapply(self$getX,self$sources)
       self$yValues <-mapply(self$getY,self$sources)
-      if(simPeriod){# only simPeriod curves
-        dsassert(is.paramrange(model$range),"Model must have a paramRange to use simPeriod=TRUE.")
-        #find the periods
-        args=append(self$find.period.args,list(FUN=model$find.period,x=self$testX,y=self$testY))
-        self$aname=model$range$aname
-        self$bname=model$range$bname
-        args[[self$aname]]=self$xValues
-        args[[self$bname]]=self$yValues
-        periods=do.call(what=mapply,args=args)
-        #break into phases (transitions)
-        transitions = rle(periods)
-        p = cumsum(transitions$lengths)
-        n = length(p)
-        starts = c(1,(p+1)[-n])
-        ends = p
-        self$phaseFrame = data.frame(start  = self$sources[starts],
-                                     period = transitions$values,
-                                     stop   = self$sources[ends])
-        #if(self$narrowFlag){       # if we want to have the option to autimatically narrow
-        # do the stuff in recalculate
-        #}
-
-        #chose colors
-        self$makeColMap()
-        self$toPlot = vector("list", length=length(ends))
-        self$col = vector(length=length(ends))
-        for(i in 1:length(ends)) {
-          phase = starts[i]:ends[i]
-          self$toPlot[[i]] = data.frame(x = self$xValues[phase], y = self$yValues[phase])
-          p = transitions$values[[i]]
-          self$col[[i]] = self$colMap[[as.character(p)]]
-        }
+      if(simPeriod) {# only simPeriod curves
+        self$buildSimPlots()
       } else { #only not sim Period curves
         self$toPlot <- model$apply(self$xValues, self$yValues, iters=self$iters, crop = self$crop)
+      }
+    },
+    render = function(self, model) {
+      if(display){
+        if(self$discretize){
+          for(i in 1:length(self$toPlot))
+            points(self$toPlot[[i]]$x, self$toPlot[[i]]$y,
+                   col = self$col[[i]], ... = self$...)
+        }
+        else{
+          for(i in 1:length(self$toPlot))
+            lines(self$toPlot[[i]]$x, self$toPlot[[i]]$y, lwd = self$lwd,
+                  col = self$col[[i]], ... = self$...)
+        }
       }
     },
     recalculate = function(self, model) {
@@ -304,89 +239,201 @@ dscurve <- function(fun, yfun = NULL,
         self$on.bind(model)
       }
     },
+    makeSources = function(self, model){
+      if(is.null(self$n)) #get numPoints
+        numPoints <- model$range$renderCount
+      else
+        numPoints <- self$n
+      if(is.null(self$lims)){ #get limits if we dont have them
+        if(is.paramrange(model$range)) #if we have a paramRange, we want the alim.
+          self$lims=model$range$alim
+        else                           #if not, we want the xlim
+          self$lims=model$range$xlim
+      }
+      from=min(self$lims)  #make a sequence
+      to=max(self$lims)
+      self$sources <- seq(from,to, length.out = numPoints)
+    },
+    buildFunctions = function(self,model){ #determining how to pick x and y values
+      if(self$isParametric){
+        getX <- ensureFunction(self$fun, TRUE)
+        getY <- ensureFunction(self$yfun, TRUE)
+      }
+      else{ #not parametric curve
+        getX <- identity
+        getY <- ensureFunction(self$fun, FALSE)
+        if(is.paramrange(model$range) && !safe.apply(is.function, eval(self$fun))) {
+          #param model and getX is an expression, figure out parameter name.
+          subNames=all.names(self$fun)
+          ain=model$range$aname %in% subNames
+          xin="x" %in% subNames
+          if(!xin || ain){
+            names(formals(getY))=model$range$aname
+            if(xin && ain) {
+              warning(paste("curve function contains both 'x' and'", self$sourceName, "'. Assuming you want to vary on ",self$sourceName,"."))
+            }
+          }
+        }
+      }
+      self$sourceName=names(formals(getY))[[1]]
+      self$getX=getX
+      self$getY=getY
+    },
+    buildSimPlots = function(self) {
+      dsassert(self$simPeriod, "Simulation can only be used on dscurves constructed with simPeriod=TRUE")
+      dsassert(is.paramrange(self$model$range),"Model must have a paramRange to use simPeriod=TRUE")
+      #find the periods
+      args=append(self$find.period.args,list(FUN=model$find.period,x=self$testX,y=self$testY))
+      self$aname=self$model$range$aname
+      self$bname=self$model$range$bname
+      args[[self$aname]]=self$xValues
+      args[[self$bname]]=self$yValues
+      periods=do.call(what=mapply,args=args)
+      #break into phases (transitions)
+      transitions = rle(periods)
+      p = cumsum(transitions$lengths)
+      n = length(p)
+      starts = c(1,(p+1)[-n])
+      ends = p
+      self$phaseFrame = data.frame(start  = self$sources[starts],
+                                   period = transitions$values,
+                                   stop   = self$sources[ends])
+      #if(self$narrowFlag){       # if we want to have the option to autimatically narrow
+      # do the stuff in recalculate
+      #}
+
+      #chose colors
+      self$makeColMap()
+      self$toPlot = vector("list", length=length(ends))
+      self$col = vector(length=length(ends))
+      for(i in 1:length(ends)) {
+        phase = starts[i]:ends[i]
+        self$toPlot[[i]] = data.frame(x = self$xValues[phase], y = self$yValues[phase])
+        p = transitions$values[[i]]
+        self$col[[i]] = self$colMap[[as.character(p)]]
+      }
+    }
     makeColMap = function(self) {
       maxPeriod=max(self$phaseFrame[,"period"])
       #only runs if current map is to small/ missing maxPeriod
       if(maxPeriod+2>length(self$colMap)){ #or if(is.null(self$colMap[[as.character(maxPeriod)]])){
         powersOf2=self$find.period.args$powersOf2
-        if(is.null(powersOf2)){
+        if(is.null(powersOf2))
           powersOf2=TRUE
-        }
-        if(powersOf2){
+        if(powersOf2)
           numCol=log(maxPeriod,2)+2
-        }
-        else{
+        else
           numCol=maxPeriod+2
-        }
         #slightly darker version of simmapperiod's colors
         if(is.null(self$givenColors) || length(self$givenColors)<numCol){
-          if (numCol <= 6)
+          if(!is.null(self$givenColors))
+            warning("not enough colors given, using a preset") #warning? More colors needed
+          if (numCol <= 6) {
             self$col <- darken(c("yellow", "magenta", "orange", "green", "red", "blue"))
-          else if (numCol <= 28)
+          } else if (numCol <= 28) {
             self$col <- darken(c("#00119c","#cdff50","#8d00a9","#00b054","#ff40dd","#01f9be","#ff1287",
                                  "#2a73ff","#d99b00","#f5ff84","#3e004a","#91fffa","#ff455a","#00a5f3",
                                  "#850f00","#9897ff","#0e2100","#e2b5ff","#005238","#ffa287","#12002c",
                                  "#e2ffe0","#620045","#ffd3e1","#2b0a00","#0068b0","#5f1800","#00376f"))
-          else
+          } else {
             self$col <- rainbow(numCol)
-          if(!is.null(self$givenColors)){
-            warning("not enough colors given, using a preset") #warning? More colors needed
           }
         }
         self$colMap=new.env()
         self$colMap[[as.character(0)]]=self$col[1]
         self$colMap[[as.character(Inf)]]=self$col[numCol]
-        i=1
-        colIndex=2
-        while(i<=maxPeriod){
-          self$colMap[[as.character(i)]]=self$col[colIndex]
-          colIndex=colIndex+1
-          if(powersOf2){
+        if(powersOf2) {
+          i=1
+          colIndex=2
+          while(i<=maxPeriod){
+            self$colMap[[as.character(i)]]=self$col[colIndex]
+            colIndex=colIndex+1
             i=i*2
           }
-          else{
-            i=i+1
+        } else {
+          for(i in 1:maxPeriod) {
+            self$colMap[[as.character(i)]]=self$col[i+1]
           }
-        }
-      }
-    },
-    render = function(self, model) {
-      if(display){
-        if(self$discretize){
-          for(i in 1:length(self$toPlot))
-            points(self$toPlot[[i]]$x, self$toPlot[[i]]$y,
-                   col = self$col[[i]], ... = self$...)
-        }
-        else{
-          for(i in 1:length(self$toPlot))
-            lines(self$toPlot[[i]]$x, self$toPlot[[i]]$y, lwd = self$lwd,
-                  col = self$col[[i]], ... = self$...)
         }
       }
     },
     #functions to give data to the user
-    phaseDist=function(self, prev, post){
-      x1=self$getX(prev$stop)
-      x2=self$getX(post$start)
-      y1=self$getY(prev$stop)
-      y2=self$getY(post$start)
-      p1=c(x1,y1)
-      p2=c(x2,y2)
-      sqdist(p1,p2)
+    phases=function(self, distances=FALSE, sources=TRUE, params=FALSE){  #add or take out columns of phaseFrame according to parameters.
+      dsassert(self$bound, "To use this method the curve must be bound to a model")
+      dsassert(self$simPeriod, "To use this method the curve be constructed with simPeriod=TRUE")
+      ret=self$phaseFrame
+      if(params){
+        startA=paste("start",self$aname)
+        stopA=paste("stop",self$aname)
+        startB=paste("start",self$bname)
+        stopB=paste("stop",self$bname)
+        start=ret$start
+        stop=ret$stop
+        add=data.frame(self$getX(start),self$getY(start),self$getX(stop),self$getY(stop))
+        names(add)=c(startA,startB,stopA,stopB)
+        ret=cbind(ret,add)[c("period","start",startA,startB,"stop",stopA,stopB)]
+      }
+      if(distances){
+        ret=self$addDistanceToPhase(ret)
+      }
+      if(!sources){
+        ret[,c("start","stop")]=NULL
+      }
+      ret
+    },
+    addDistanceToPhase=function(self,inPhase){
+      findDist=function(index,phases){
+        sqrt(self$distOfSources(phases[index,]$start, phases[index,]$stop))
+      }
+      dist=mapply(findDist, 1:nrow(inPhase), MoreArgs=list(inPhase))
+      withDist=cbind(inPhase, dist)
+      findRatio=function(index,phases){
+        (phases[index,]$dist)/(phases[index+1,]$dist)
+      }
+      ratio=append(NA, mapply(findRatio, 1:(nrow(inPhase)-1), MoreArgs=list(withDist)))
+      cbind(withDist, ratio)
+    },
+    distOfSources=function(self, pointA, pointB){
+      x1=self$getX(pointA)
+      y1=self$getY(pointA)
+      x2=self$getX(pointB)
+      y2=self$getY(pointB)
+      sqdist(c(x1,y1),c(x2,y2))
+    },
+    narrow= function(self, tolerance=sqrt(sqrt(.Machine$double.eps)), redisplay=TRUE){
+      dsassert(self$bound, "To use this function the curve must be bound to a model")
+      dsassert(self$simPeriod, "To use this function the curve must have simPeriod set to true")
+      self$narrowed = TRUE
+      pf=self$phaseFrame
+      numRow=nrow(pf)
+      if(numRow>1){ #if there is anything to be narrowed
+        newPf=self$recurNarrow(pf[1,],pf[2,],tolerance)
+        if(numRow>2){ #if there is more to be narrowed
+          for(i in 2:(numRow-1)){
+            post=self$recurNarrow(pf[i,],pf[i+1,],tolerance)
+            newPf=self$phaseMerge(newPf,post)
+          }
+        }
+        self$phaseFrame=newPf
+      }
+      if(redisplay){
+        self$recalculate(self$model)
+        #self$model$redisplay() #if something should be on top of this, redisplay will keep it that way
+        self$render(self$model) #I think is should alway be on top anyways though
+      }
+      self$phaseFrame
     },
     phaseMerge= function(prev,post){
       lenPrev=nrow(prev)
       midStart=prev[lenPrev,]$start   #merge the result from both sides
       post[1,]$start=midStart
-      if(lenPrev==1){
+      if(lenPrev==1)
         return(post)
-      }
-      else{
-      return(rbind(prev[1:(lenPrev-1),],post))
-      }
+      else
+        return(rbind(prev[1:(lenPrev-1),],post))
     },
-    recurNarrow= function(self, prev,post,tolerance){
-      if(self$phaseDist(prev,post) < tolerance){ #xydist
+    recurNarrow= function(self,prev,post,tolerance){
+      if(self$distOfSources(prev$stop,post$start) < tolerance){ #xydist
         return(rbind(prev,post))
       }
       midPoint=(prev$stop+post$start)/2
@@ -415,71 +462,6 @@ dscurve <- function(fun, yfun = NULL,
         prev$stop=midPoint
       }
       return(self$recurNarrow(prev,post,tolerance))
-
-    },
-    narrow= function(self, tolerance=sqrt(sqrt(.Machine$double.eps)), redisplay=TRUE){
-      dsassert(self$bound, "To use this function the curve must be bound to a model")
-      dsassert(self$simPeriod, "To use this function the curve must have simPeriod set to true")
-      self$narrowed = TRUE
-      pf=self$phaseFrame
-      numRow=nrow(pf)
-      if(numRow>1){ #if there is anything to be narrowed
-        newPf=self$recurNarrow(pf[1,],pf[2,],tolerance)
-        if(numRow>2){ #if there is more to be narrowed
-          for(i in 2:(numRow-1)){
-            post=self$recurNarrow(pf[i,],pf[i+1,],tolerance)
-            newPf=self$phaseMerge(newPf,post)
-          }
-        }
-        self$phaseFrame=newPf
-      }
-      if(redisplay){
-        self$recalculate(self$model)
-        #self$model$redisplay() #if something should be on top of this, redisplay will keep it that way
-        self$render(self$model) #I think is should alway be on top anyways though
-      }
-      self$phaseFrame
-    },
-    addDistanceToPhase=function(self,inPhase){
-      findDist=function(index,phases){
-        x1=self$getX(phases[index,]$stop)
-        x2=self$getX(phases[index,]$start)
-        y1=self$getY(phases[index,]$stop)
-        y2=self$getY(phases[index,]$start)
-        sqrt((x1-x2)^2 + (y1-y2)^2)
-      }
-      dist=mapply(findDist,1:nrow(inPhase),MoreArgs=list(inPhase))
-      withDist=cbind(inPhase,dist)
-      findRatio=function(index,phases){
-        (phases[index,]$dist)/(phases[index+1,]$dist)
-      }
-      ratio=append(NA,mapply(findRatio,1:(nrow(inPhase)-1),MoreArgs=list(withDist)))
-      cbind(withDist,ratio)
-    },
-    phases=function(self, distances=FALSE, sources=TRUE, params=FALSE){  #add or take out columns of phaseFrame according to parameters.
-      dsassert(self$bound, "To use this function the curve must be bound to a model")
-      dsassert(self$simPeriod, "To use this function the curve must have simPeriod set to true")
-      ret=self$phaseFrame
-      if(params){
-        startA=paste("start",self$aname)
-        stopA=paste("stop",self$aname)
-        startB=paste("start",self$bname)
-        stopB=paste("stop",self$bname)
-        start=ret$start
-        stop=ret$stop
-        add=data.frame(self$getX(start),self$getY(start),self$getX(stop),self$getY(stop))
-        names(add)=c(startA,startB,stopA,stopB)
-        ret=cbind(ret,add)
-        ret=ret[c("period","start",startA,startB,"stop",stopA,stopB)]
-      }
-      if(distances){
-        ret=self$addDistanceToPhase(ret)
-      }
-
-      if(!sources){
-        ret[,c("start","stop")]=NULL
-      }
-      ret
     }
   )
 }
