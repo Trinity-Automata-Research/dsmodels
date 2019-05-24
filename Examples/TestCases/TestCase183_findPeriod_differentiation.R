58466a02a377d496bc38690ce7510c509a0e5335
kmclaren-trinity <kmclaren@gmail.com>
2019-05-24 14:14:00 -0500

    minor changes to tests

--- Examples/TestCases/TestCase183_findPeriod_differentiation.R
+++ Examples/TestCases/TestCase183_findPeriod_differentiation.R
@@ -12,9 +12,9 @@ mod + paramrange(4,4,discretize = .1, paramNames = c(s,dummy),renderCount = 20)
 mod + sim.map.period(maxPeriod = 8, epsilon=.0001, iters = 100, numTries = 1, powerOf2=TRUE)
 
 #not parametric
-c=dscurve(2, xlim=c(0,3.573555857), simPeriod = TRUE, find.period.args=list(iters=1000, maxPeriod=128, initIters=1000, numTries=5 ),col="black")
+c=dscurve(2, xlim=c(0,4), simPeriod = TRUE, find.period.args=list(iters=10000, maxPeriod=128, initIters=1000, numTries=30 ),col="black")
 mod+c
-print(c$narrow(tolerance=.00001)) #refine the ranges
+print(c$narrow(tolerance=.0000001)) #refine the ranges
 print(c$phases(distances=TRUE),digits=10)
 
 ##
