efea6d270ff3478c9f57ffe341b634234528a583
kmclaren-trinity <kmclaren@gmail.com>
2019-06-11 17:47:29 -0500

    more alt findPeriods

--- Examples/altFindPeriod.R
+++ Examples/altFindPeriod.R
@@ -22,15 +22,15 @@ find.period = function( x, y, iters=NULL, maxPeriod=128, initIters=500, numTries
   return(Inf)
 }
 
-#helper for classical unique bucket
+#helper for unique bucket
 #in 1d could sort first then find in one pass.
 #in general, need n! comparisons
-numBuckets=function(points, epsilon=sqrt(.Machine$double.eps)){ #removes all elements close to the first, repeats
+numBuckets=function(candidates, epsilon=sqrt(.Machine$double.eps)){ #removes all elements close to the first, repeats
   bucket=0
-  while(length(points)>0){
-    test=points[1]
-    dists=mapply(sqdist,test,points)
-    points[dists < epsilon]=NULL
+  while(length(candidates)>0){
+    test=candidates[1]
+    dists=mapply(sqdist,test,candidates)
+    candidates[dists < epsilon]=NULL
     bucket=bucket+1
   }
   bucket
@@ -55,6 +55,10 @@ find.period = function( x, y, iters=NULL, maxPeriod=128, initIters=500, numTries
     #print("no period found, diverged")
     return(FALSE)
   }
+  check124(candidates,epsilon,maxPeriod)
+}
+
+check124=function(candidates,epsilon,maxPeriod){
   test=candidates[[1]]
   period=FALSE
   j=1
@@ -74,9 +78,86 @@ find.period = function( x, y, iters=NULL, maxPeriod=128, initIters=500, numTries
 }
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~
+#classical, whole period. if default is null, is unused
+find.period = function( x, y, iters=NULL, maxPeriod=128, initIters=500, numTries=NULL, powerOf2=TRUE,
+                        epsilon=sqrt(.Machine$double.eps), convergeCheck=NULL, ...){
+  #moves all the points. stops if they are either all infinite, fixed, or if(crop==TRUE), outside of range
+
+  startPoint <- apply(x=x,y=y,...,iters=initIters,accumulate=FALSE)
+
+  x=startPoint$x
+  y=startPoint$y
+
+  candidates=apply(x, y, ...,iters=maxPeriod*2-1,accumulate=TRUE)
+  last=candidates[[2*maxPeriod]]
+  if(has.diverged(last$x,last$y)){  #check if points became inf
+    #print("no period found, diverged")
+    return(FALSE)
+  }
+  return(wholePeriodCheck(candidates,epsilon,maxPeriod,powerOf2)) #any period 128 or greater is called 128
+  return(Inf)
+}
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~
-#dsmodels. if default is null, is unused
+#dsmodels current- convergeCheck, wholePeriod. if default is null, is unused
+#for each pass, prints true or false for if it converged
+find.period = function( x, y, iters=NULL, maxPeriod=128, initIters=100, numTries=5, powerOf2=TRUE,
+                        epsilon=sqrt(.Machine$double.eps), convergeCheck=NULL, ...){
+  #moves all the points. stops if they are either all infinite, fixed, or if(crop==TRUE), outside of range
+
+  for(i in 1:numTries) {
+    startPoint <- apply(x=x,y=y,...,iters=initIters,accumulate=FALSE)
+    candidates=apply(startPoint$x, startPoint$y, ...,iters=maxPeriod*2-1,accumulate=TRUE)
+    #puts a gap between the two parts to check
+    #convergePoint <- apply(startPoint$x,startPoint$y,...,iters=convergeCheck,accumulate=FALSE)
+
+    #puts no gap between the two parts to check
+    convergePoint <- apply(candidates[[maxPeriod*2]]$x, candidates[[maxPeriod*2]]$y,...,iters=1,accumulate=FALSE)
+    compareCandidates= apply(convergePoint$x, convergePoint$y, ...,iters=maxPeriod*2-1,accumulate=TRUE)
+    last=compareCandidates[[maxPeriod*2]]
+    if(has.diverged(last$x,last$y)){
+      #print("no period found, diverged")
+      return(FALSE)
+    }
+    dists=mapply(sqdist,candidates,compareCandidates)
+    print(sum(dists) < epsilon)
+    #check if function has converged. if it has check for periodicity, otherwise go to next pass of this loop. exception:
+    if(i==numTries || sum(dists) < epsilon){  # check for periodicity should always happen if i=numTries.i.e. if on last try, continue anyways
+      return(wholePeriodCheck(candidates,epsilon,maxPeriod,powerOf2))
+    }
+    #update x,y to a point further in the orbit
+
+    x=last$x
+    y=last$y
+  }
+  return(Inf)
+}
+
+#check 1:2^n against 2^n+1:2*2^n
+wholePeriodCheck=function(candidates,epsilon,maxPeriod,powerOf2){
+  period=FALSE
+  j=1
+  while(j<=maxPeriod && !period){ #check for fixed or periodicity
+    test=candidates[1:j]
+    image=candidates[(j+1):(2*j)]
+    dists=mapply(sqdist,test,image)
+    if(all(dists < epsilon))
+      period=TRUE
+    else{
+      if(powerOf2)
+        j=2*j
+      else
+        j=j+1
+    }
+  }
+    return(j)
+}
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~
+#convergeCheck, unique buckets. if default is null, is unused
 #for each pass, prints true or false for if it converged
 find.period = function( x, y, iters=NULL, maxPeriod=128, initIters=100, numTries=5, powerOf2=TRUE,
                         epsilon=sqrt(.Machine$double.eps), convergeCheck=NULL, ...){
@@ -100,24 +181,7 @@ find.period = function( x, y, iters=NULL, maxPeriod=128, initIters=100, numTries
     print(sum(dists) < epsilon)
     #check if function has converged. if it has check for periodicity, otherwise go to next pass of this loop. exception:
     if(i==numTries || sum(dists) < epsilon){  # check for periodicity should always happen if i=numTries.i.e. if on last try, continue anyways
-      period=FALSE
-      j=1
-      while(j<=maxPeriod && !period){ #check for fixed or periodicity
-        test=candidates[1:j]
-        image=candidates[(j+1):(2*j)]
-        dists=mapply(sqdist,test,image)
-        if(all(dists < epsilon))
-          period=TRUE
-        else{
-          if(powerOf2)
-            j=2*j
-          else
-            j=j+1
-        }
-      }
-      if(period){
-        return(j)
-      }
+      return(numBuckets(candidates,epsilon)) #any period 128 or greater is called 128
     }
     #update x,y to a point further in the orbit
 
