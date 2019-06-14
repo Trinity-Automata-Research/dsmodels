e0f75e84d052d1f3493da3942b3e3120ee4d3e78
kmclaren-trinity <kmclaren@gmail.com>
2019-06-14 16:10:26 -0500

    more comparing to maple

--- Examples/compareToMaple.R
+++ Examples/compareToMaple.R
@@ -1,3 +1,6 @@
+#500 ir 1000 its is not enough
+#try 200000 or something?
+
 f=function(x,a) a*x*(1-x)
 
 x=.5
@@ -58,3 +61,106 @@ narrow=function(starta,startp,enda,endp, narrowTol){
 
 print(narrow(2.9,1,3.1,2,narrowTol),digits=15)
 print(narrow(3.4,2,3.5,4,narrowTol),digits=15)
+print(narrow(3.53,4,3.55,8,narrowTol),digits=15)
+print(narrow(3.55,8,3.566,16,narrowTol),digits=15)
+print(narrow(3.566,16,3.569,32,narrowTol),digits=15)
+
+# with a=1, should converge to 0   this is close to a critical point
+iter(x,a,its)
+iter(x,a,its*5)
+iter(x,a,its*10)
+orbit=iter(x,a,100000)
+
+for(i in 1:20){
+  print(orbit)
+  orbit=iter(orbit,a,1)
+}
+
+# with a=2, should converge to .5   this is far from a critical point
+a=2
+iter(x,a,its)
+iter(x,a,its*5)
+iter(x,a,its*10)
+orbit=iter(x,a,100000)
+
+for(i in 1:20){
+  print(orbit)
+  orbit=iter(orbit,a,1)
+}
+
+
+# with a=3, should converge to 2/3   this is a critical point
+a=3
+iter(x,a,its)
+iter(x,a,its*5)
+iter(x,a,its*10)
+orbit=iter(x,a,100000)
+
+for(i in 1:20){
+  print(orbit,digits=15)
+  orbit=iter(orbit,a,1)
+}
+
+
+#go out way further-  1 billion iterations. this isnt far enough?
+#who cares about rounding error- all points converge to the same orbit
+#we need a way to tell the difference between eventually fixed and periodic
+#without iterating untill its fixed
+orbit=iter(x,a,1000000000)
+longOrbit=orbit
+for(i in 1:20){
+  print(orbit,digits=15)
+  orbit=iter(orbit,a,1)
+}
+#1bil more
+orbit=iter(orbit,a,1000000000)
+
+
+
+a=2.999
+iter(x,a,its)
+iter(x,a,its*5)
+iter(x,a,its*10)
+orbit=iter(x,a,100000)
+print(orbit,digits=20)
+
+for(i in 1:20){
+  print(orbit,digits=15)
+  orbit=iter(orbit,a,1)
+}
+
+
+a=3.000001
+iter(x,a,its)
+iter(x,a,its*5)
+iter(x,a,its*10)
+orbit=iter(x,a,100000)
+print(orbit,digits=20)
+
+for(i in 1:20){
+  print(orbit,digits=15)
+  orbit=iter(orbit,a,1)
+}
+
+
+
+#for a's close to 3, how fast does a converge
+a=3
+its=10000000
+mu=a
+orbit=iter(x,mu,its)
+orbit2=iter(orbit,mu,2)
+# print(orbit,digits=15)
+#  print(orbit2,digits=15)
+print(round(log(abs(orbit-orbit2),10)))
+
+
+for(i in 1:8){
+  #print(a-.1^i,digits=15)
+  mu=a-.1^i
+  orbit=iter(x,mu,its)
+  orbit2=iter(orbit,mu,2)
+ # print(orbit,digits=15)
+#  print(orbit2,digits=15)
+    print(round(log(abs(orbit-orbit2),10)))
+}
