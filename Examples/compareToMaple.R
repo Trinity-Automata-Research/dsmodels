fc3afb89008ec32e0e56bb88c5e8a1d512934304
kmclaren-trinity <kmclaren@gmail.com>
2019-06-19 17:56:12 -0500

    found how to find bifurcation points

--- Examples/compareToMaple.R
+++ Examples/compareToMaple.R
@@ -5,9 +5,10 @@ f=function(x,a) a*x*(1-x)
 
 x=.5
 a=1
-its=1000000
-print(its)
+maxIts=1e6
+stride=2^20
 tol=1.0e-8
+cTol=.Machine$double.eps^(2/3)
 narrowtol=1.0e-8
 
 iter=function(x,a,its){
@@ -22,19 +23,41 @@ iter=function(x,a,its){
 # for(i in 1:6-3){print(iter(x,a,its+i))}
 
 eq=function(x1,x2,tol){
-  (x1-x2)^2<tol
+  abs(x1-x2)<tol
 }
 
-findPer=function(x,a,its,tol,maxP){
-  n=iter(x,a,its)
+findPerConv=function(x=x,a=a,stride=stride, maxIts=maxIts,tol=tol,maxP=5){
+  old=x
+  x=iter(x,a,stride)
+  its=stride
+  while(its<maxIts && !eq(old,x,cTol)){
+    old=x
+    x=iter(x,a,stride)
+    its=its+stride
+  }
+  #print(c(x,old,x-old),digits=22)
   p=Inf
   for(i in 0:maxP){
-    if(eq(n,iter(n,a,2^i),tol)){
+    if(eq(x,iter(x,a,2^i),tol)){
       p=2^i
       break
     }
   }
-  p
+  c(p,its)
+}
+
+findPer=function(x,a,stride, maxIts,tol,maxP){
+
+  x=iter(x,a,maxIts)
+
+  p=Inf
+  for(i in 0:maxP){
+    if(eq(x,iter(x,a,2^i),tol)){
+      p=2^i
+      break
+    }
+  }
+  c(p,its)
 }
 
 #
@@ -46,21 +69,44 @@ findPer=function(x,a,its,tol,maxP){
 
 
 
-narrow=function(starta,startp,enda,endp, narrowTol){
+narrow=function(starta,startp,enda,endp, narrowTol, its=0 ){
   if(abs(starta-enda)<narrowTol){
-    c(starta,enda)
+    c(starta,enda,its)
   }else{
     mid=(starta+enda)/2
-    midp=findPer(x,mid,its,tol,5)
+    res=findPerConv(x,mid,stride,maxIts,tol,10)
+    midp=res[1]
+    its=max(its,res[2])
     if(midp==startp){
-      narrow(mid,startp,enda,endp,narrowTol)   #narrow((mid+starta)/2,startp,enda,endp,narrowTol)
+      narrow(mid,startp,enda,endp,narrowTol,its)   #narrow((mid+starta)/2,startp,enda,endp,narrowTol)
     }else{
-      narrow(starta,startp,mid,endp,narrowTol)  #narrow(starta,startp,(mid+enda)/2,endp,narrowTol)
+      narrow(starta,startp,mid,endp,narrowTol,its)  #narrow(starta,startp,(mid+enda)/2,endp,narrowTol)
     }
   }
 }
 
-b2=narrow(2.9,1,3.1,2,narrowTol)
+feigen=function(p,min,max){
+  n=length(p)
+  print("with converge check")
+  ptm <- proc.time()
+  bif=mapply(narrow, starta=min, startp=p, enda=max, endp=2*p, narrowTol )
+  print(proc.time() - ptm)
+
+  dif= bif[1,-1]-bif[1,-n]
+  ratio= dif[-(n-1)]/dif[-1]
+
+  data.frame(period=p,pointMin=bif[1,], pointMax=bif[2,],dist=c(NA,dif), fieg=c(NA,NA, ratio), its=bif[3])
+}
+
+p=c(1,2,4,8,16,32,64,128)
+min=c(2.999,3.4,3.53,3.55,3.566,3.569,3.5697,3.5699)
+max=c(3.001,3.5,3.55,3.566,3.569,3.5697,3.5699,3.56994)
+df=feigen(p,min,max)
+print(df)
+
+
+if(FALSE){
+b2=narrow(2.999,1,3.001,2,narrowTol)
 print(b2,digits=15)
 b4=narrow(3.4,2,3.5,4,narrowTol)
 print(b4,digits=15)
@@ -70,10 +116,35 @@ b16=narrow(3.55,8,3.566,16,narrowTol)
 print(b16,digits=15)
 b32=narrow(3.566,16,3.569,32,narrowTol)
 print(b32,digits=15)
+b64=narrow(3.569,32,3.5697,64,narrowTol)
+print(b64,digits=15)
+b128=narrow(3.5697,64,3.5699,128,narrowTol)
+print(b128,digits=15)
+b256=narrow(3.5699,128,3.56994,256,narrowTol)
+print(b256,digits=15)
 
 print((b2[1]-b4[1])/(b4[1]-b8[1]),digits=15)
 print((b4[1]-b8[1])/(b8[1]-b16[1]),digits=15)
 print((b8[1]-b16[1])/(b16[1]-b32[1]),digits=15)
+print((b16[1]-b32[1])/(b32[1]-b64[1]),digits=15)
+print((b32[1]-b64[1])/(b64[1]-b128[1]),digits=15)
+print((b64[1]-b128[1])/(b128[1]-b256[1]),digits=15)
+}
+
+# mu=2.9999
+# findPer(x,mu,stride,maxIts,tol,10)
+# orbit=0.66665555518485342
+# for(i in 1:20){
+#    print(orbit,digits=15)
+#    orbit=iter(orbit,mu,1)
+# }
+# orbit=iter(orbit,mu,10000000)
+# for(i in 1:20){
+#   print(orbit,digits=15)
+#   orbit=iter(orbit,mu,1)
+# }
+#
+# v=mapply(iter,its=1:10, MoreArgs=list(orbit,mu))
 
 #
 # # with a=1, should converge to 0   this is close to a critical point
